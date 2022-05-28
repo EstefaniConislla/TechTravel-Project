5b6adedf9d9105a163d872c2887484497924f7d1
EstefaniConislla <stefani_19_94@hotmail.com>
2022-05-28 12:46:47 -0500

    TechTravel - routes

---
+++ tech-travel-application/src/Routes/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+import Cart from "../Pages/Cart";
+import Home from "../Pages/Home";
+
+const Routing = () => {
+    return(
+
+    )
+}
